bc91cd50ad010b072011c50c5133453a34c7f80d
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-02-08 21:32:36 +0200

    fix: update import syntax in globals.css and add suppressHydrationWarning to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} m-0 box-border p-0 antialiased`}>
         <ThemeProvider
           attribute="class"
